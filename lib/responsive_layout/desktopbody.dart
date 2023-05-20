1378d4634cbeeae532caf9842c8536635fa9e254
Abhinandan <abhinandhannandrekar@gmail.com>
2023-05-20 17:26:49 +0530

    all data has been commited

--- lib/responsive_layout/desktopbody.dart
+++ lib/responsive_layout/desktopbody.dart
@@ -6,46 +6,40 @@ class MydesktopBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: AspectRatio(
-                      aspectRatio: 16 / 9,
-                      child: Container(
-                        //width: 250,
-                        color: Colors.green,
-                      ),
+      body: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Container(
+                      //width: 250,
+                      color: Colors.green,
                     ),
                   ),
-                  Expanded(
-                      child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+                ),
+                Expanded(
                     child: ListView.builder(
                         itemCount: 8,
                         itemBuilder: ((context, index) {
                           return Container(
                             padding: const EdgeInsets.all(8.0),
-                            height: 120,
+                            height: 100,
                             color: Colors.lightGreen,
                           );
-                        })),
-                  ))
-                ],
-              ),
+                        })))
+              ],
             ),
-            // second column
-            Container(
-              padding: EdgeInsets.all(10),
-              width: 200,
-              color: Colors.blue,
-            )
-          ],
-        ),
+          ),
+          // second column
+          Container(
+            width: 200,
+            color: Colors.blue,
+          )
+        ],
       ),
     );
   }
